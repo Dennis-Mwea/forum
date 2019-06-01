0387fa3ded872d1cb5c8022876c01acc235b0657
Dennis Mwea <mweadennis2@gmail.com>
2019-06-02 02:10:24 +0300

    Rename controllers

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Thread;
 use Illuminate\Http\Request;
 
-class ThreadController extends Controller
+class ThreadsController extends Controller
 {
     /**
      * Display a listing of the resource.
