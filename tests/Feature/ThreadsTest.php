b5a84efd53274f0bd95501ff21bde79374d888f3
Dennis Mwea <mweadennis2@gmail.com>
2019-06-02 01:57:39 +0300

    Created Threads test class

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class ThreadsTest extends TestCase
 {
     /**
      * A basic test example.
