c06cb546188808016290225409a411737fd7fd83
Dennis Mwea <mweadennis2@gmail.com>
2019-06-02 02:13:22 +0300

    Added threads link

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -14,7 +14,11 @@ class ThreadsController extends Controller
      */
     public function index()
     {
-        //
+        $threads = Thread::latest()->get();
+
+        return view('threads.index', [
+            'threads' => $threads
+        ]);
     }
 
     /**
