a251335002fc308a7844c2d5792cce0837856850
Dennis Mwea <mweadennis2@gmail.com>
2019-06-02 03:47:15 +0300

    Test a reply has an owner

---
+++ tests/Unit/ReplyTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use App\Reply;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ReplyTest extends TestCase
+{
+    use DatabaseMigrations;
+    
+    /**
+     * Test whether a reply has an owner
+     * @test
+     */
+    public function itHasAnOwner()
+    {
+        $reply = factory(Reply::class)->create();
+
+        $this->assertInstanceOf(User::class, $reply->user);
+    }
+}
