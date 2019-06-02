028f90ae27deb74b346e7c92c16527d824c8a43d
Dennis Mwea <mweadennis2@gmail.com>
2019-06-02 05:00:29 +0300

    Added the add reply form.

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
                 <div class="card-header">
                     {{ $thread->user->name }} posted 
@@ -22,11 +22,31 @@
     </div>
 
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             @foreach ($thread->replies as $reply)
                 @include('threads.replies')
             @endforeach
         </div>
     </div>
+
+    @if (auth()->check())
+        <div class="row justify-content-center replies">
+            <div class="col-md-10">
+                <form action="{{ $thread->path() . '/replies' }}" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label for="body">Body:</label>
+                        <textarea name="body" id="body" rows="5" class="form-control"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Reply</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    @else
+        <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+    @endif
 </div>
 @endsection
