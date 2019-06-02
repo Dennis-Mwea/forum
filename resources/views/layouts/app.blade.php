201995ccb2cf5b31a6fed2365c9f09697532220a
Dennis Mwea <mweadennis2@gmail.com>
2019-06-02 05:03:18 +0300

    Link back to all threads page.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <a class="nav-link" href="/threads">All threads</a>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
