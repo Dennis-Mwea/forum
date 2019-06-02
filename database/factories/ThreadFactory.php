744b143bd39ef406b2a5e948958a169a2c14bc7f
Dennis Mwea <mweadennis2@gmail.com>
2019-06-02 06:54:02 +0300

    Add channel field to thread factory.

--- database/factories/ThreadFactory.php
+++ database/factories/ThreadFactory.php
@@ -4,12 +4,16 @@
 
 use App\User;
 use App\Thread;
+use App\Channel;
 use Faker\Generator as Faker;
 
 $factory->define(Thread::class, function (Faker $faker) {
     return [
         'user_id' => function () {
             return factory(User::class)->create()->id;
+        },
+        'channel_id' => function () {
+            return factory(Channel::class)->create()->id;
         },
         'title' => $faker->sentence,
         'body' => $faker->paragraph(2),
