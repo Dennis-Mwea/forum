dc8aac946990038a7538f7e9ff48a233f2e8556b
Dennis Mwea <mweadennis2@gmail.com>
2019-06-04 02:23:12 +0300

    Show riplies count on the main page.

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -16,9 +16,15 @@
 
                     @foreach ($threads as $thread)
                         <article>
-                            <h4>
-                                <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                            </h4>
+                            <div class="level">
+                                <h4 class="flex">
+                                    <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
+                                </h4>
+
+                                <a href="{{ $thread->path() }}">
+                                    <strong>{{ $thread->replies_count }} {{  str_plural('reply', $thread->replies_count) }}</strong>
+                                </a>
+                            </div>
                             <div class="body">
                                 {{ $thread->body }}
                             </div>
