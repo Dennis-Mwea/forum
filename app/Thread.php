0726c8cfade9a134637b96b5068a4889685374bb
Dennis Mwea <mweadennis2@gmail.com>
2019-06-13 09:31:43 +0300

    Delete thread replies on deleting a thread.

--- app/Thread.php
+++ app/Thread.php
@@ -27,6 +27,10 @@ class Thread extends Model
         static::addGlobalScope('replyCount', function ($builder) {
             $builder->withCount('replies');
         });
+
+        static::deleting(function ($thread) {
+            $thread->replies()->delete();
+        });
     }
 
     /**
