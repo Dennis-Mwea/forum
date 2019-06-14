cf24a1aba97354f4a105397a8ee21c6992664ff8
Dennis Mwea <mweadennis2@gmail.com>
2019-06-14 09:01:08 +0300

    Migration and modelto store activities.

---
+++ app/Activity.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Activity extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'subject_id', 'subject_type', 'type'
+    ];
+
+    public function subject()
+    {
+        return $this->morphTo();
+    }
+}
